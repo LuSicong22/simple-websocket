1fc22b87722f06cf40fc359e0847d9cd647439f6
simon <simon.lu@tron.network>
2024-04-12 11:21:29 +0800

    first commit

---
+++ server.js
@@ -0,0 +1,12 @@
+const WebSocket = require("ws");
+
+const wss = new WebSocket.Server({ port: 8080 });
+
+wss.on("connection", function connection(ws) {
+  console.log("A new client connected");
+
+  ws.on("message", function incoming(message) {
+    console.log("Received message:", message);
+    ws.send(message);
+  });
+});
